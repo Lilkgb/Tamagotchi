00353ecb671b5e329500c25ba5e0be48e1bf1747
Ryan McLean and Victoria Martinez <victoria@>
2019-03-19 16:16:17 -0700

    add giphy api to project and set randomizer for image to pop up on screen

--- src/main.js
+++ src/main.js
@@ -13,12 +13,19 @@ tam.setPlay();
 console.log(tam.foodLevel);
 console.log(tam.sleep);
 console.log(tam.game);
-setInterval(() => {
+const textInterval = setInterval(() => {
   $(".foodCount").text(tam.foodLevel)
+  if (tam.foodLevel === "Tamagotchi is DEAD!") {
+    clearInterval(textInterval);
+  }
 }, 100);
 
-setInterval(() => {
+const sleepInterval = setInterval(() => {
   $(".dream").text(tam.sleep)
+  if (tam.sleep === "ZZZZZZZZZ") {
+    clearInterval(sleepInterval);
+  }
+  console.log(tam.sleep);
 }, 100);
 
 setInterval(() => {
@@ -38,4 +45,20 @@ $(".tam-sleep").click(function() {
 $(".tam-play").click(function() {
   const bleh = tam.playSmall("Let's Play!");
   $(".ball").text(bleh);
+});
+
+$(document).ready(function() {
+    $.ajax({
+        url: `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=tamagotchi&limit=25&offset=0&rating=G&lang=en`,
+        type: `GET`,
+        data: {
+          format: 'json'
+        },
+        success: function(response) {
+          $(".whatever").html(`<img src='${response.data[Math.floor(Math.random() * 25)].images.original.url}'></img>`)
+        },
+        error: function() {
+          $('#errors').text("there was an error pricessing your request. Please try again.");
+      }
+    });
 });
